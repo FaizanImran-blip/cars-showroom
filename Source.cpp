993c928c61c8ab63651966eb206ea4e8893aa89b
Faizanimran <f2024376210@umt.edu.pk>
2025-04-26 18:43:20 +0500

    new

--- Source.cpp
+++ Source.cpp
@@ -1,38 +1,36 @@
-#include<iostream>
-#include<fstream>
-#include<vector>
+#include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
-class Cars {
+
+class Car {
 public:
 	int size;
 	string name;
 	string model;
-	Cars(int s, string n, string m) :size(s), name(n), model(m)
-	{
+
+	Car(int s, string n, string m) : size(s), name(n), model(m) {
 		fstream file;
 		file.open("cars.collection", ios::out | ios::app);
-
 		file << size << "*" << name << "*" << model << "*" << endl;
-
 		file.close();
-
 	}
+
 	void show();
 	void dis();
-	void add(vector<Cars>& car);
-	void rem(vector<Cars>& car);
-
+	void add(vector<Car>& ca);
+	void rem(vector<Car>& ca);
 };
-void Cars::dis()
-{
-	int h=15;
-	for(int i=0;i<h;i++)
-	{
-		cout<<"||";
-		cout<<"\t\t\t\t\t\t\t||"<<endl;
+
+void Car::dis() {
+	int h = 15;
+	for (int i = 0; i < h; i++) {
+		cout << "||";
+		cout << "\t\t\t\t\t\t\t||" << endl;
 	}
 }
-void Cars::show() {
+
+void Car::show() {
 	fstream file;
 	int a;
 	string nam;
@@ -40,86 +38,94 @@ void Cars::show() {
 	file.open("cars.collection", ios::in);
 	cout << "Available cars in showroom:" << endl;
 
-	while (file >> a)
-	{
+	while (file >> a) {
 		file.ignore();
 		getline(file, nam, '*');
 		getline(file, mod, '*');
 		cout << "Car Name: " << nam << " | Size: " << a << " | Model: " << mod << endl;
 	}
-
 	file.close();
 }
-void Cars::add(vector<Cars>& car)
-{
+
+void Car::add(vector<Car>& ca) {
 	string na;
 	string mode;
 	int si;
-	cout << "enter the name of the car you want to add:" << endl;
+	cin.ignore(); // Clear input buffer
+	cout << "Enter the name of the car you want to add:" << endl;
 	getline(cin, na);
-	cout << "enter the model of this car:" << endl;
+	cout << "Enter the model of this car:" << endl;
 	getline(cin, mode);
-	cout << "enter the size of the car" << endl;
+	cout << "Enter the size of the car:" << endl;
 	cin >> si;
-	car.push_back(Cars(si, na, mode));
+	ca.push_back(Car(si, na, mode));
 }
-void Cars::rem(vector<Cars>& car)
-{
+
+void Car::rem(vector<Car>& ca) {
 	string nam;
-	cout << "enter the car you want to remove:" << endl;
+	cout << "Enter the car you want to remove:" << endl;
 	cin.ignore();
 	getline(cin, nam);
-	for (auto i = car.begin();i != car.end();++i)
-	{
-		if (i->name == nam)
-		{
-			car.erase(i);
-			cout << "removed successfully:" << nam << endl;
-		}
-		else {  
-			cout << "car not found:" << endl;
+
+	bool found = false;
+	for (auto i = ca.begin(); i != ca.end(); ++i) {
+		if (i->name == nam) {
+			ca.erase(i);
+			cout << "Removed successfully: " << nam << endl;
+			found = true;
+			break;
 		}
 	}
+	if (!found) {
+		cout << "Car not found." << endl;
+	}
 }
-int main()
-{
-	cout << "we can add the cars first and then you can select your car:" << endl;
-	cout << "press enter to add the cars in the showroom" << endl;
+
+int main() {
+	cout << "We can add the cars first and then you can select your car:" << endl;
+	cout << "Press enter to add the cars in the showroom" << endl;
 	cin.ignore();
-	vector<Cars>car;
-	car[0].dis();
-	car.push_back(Cars(4, "ferrari", "2014"));
-	car.push_back(Cars(5, "mehran", "2023"));
-	car.push_back(Cars(9, "alto", "2013"));
-	int c;
+
+	vector<Car> ca;
+	Car temp(0, "", "");
+	temp.dis();
+
+	ca.push_back(Car(4, "ferrari", "2014"));
+	ca.push_back(Car(5, "mehran", "2023"));
+	ca.push_back(Car(9, "alto", "2013"));
+
+	int choice;
 	string ch = "yes";
-	cout << "Now your choice to book your car from the showroom of cars:" << endl;
-	cout << "1)--book order:" << endl;
-	cout << "2)--add the car: " << endl;
-	cout << "3)--remove the car:" << endl;
-	cout << "4)--exit showroom:" << endl;
+
 	while (ch == "yes" || ch == "Yes" || ch == "YES") {
-		cin >> c;
-		switch (c)
-		{
+		cout << "\nNow your choice to book your car from the showroom of cars:\n";
+		cout << "1) Book order\n";
+		cout << "2) Add the car\n";
+		cout << "3) Remove the car\n";
+		cout << "4) Exit showroom\n";
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+		switch (choice) {
 		case 1:
-			car[0].show();
+			ca[0].show();
 			break;
 		case 2:
-			car[0].add(car);
+			ca[0].add(ca);
 			break;
 		case 3:
-			car[0].rem(car);
+			ca[0].rem(ca);
 			break;
-			case 4:
-			cout<<"exiting showroom!"<<endl;
-			default:
-			cout<<"invalid correction:::"<<endl;
-
+		case 4:
+			cout << "Exiting showroom!" << endl;
+			return 0;
+		default:
+			cout << "Invalid option. Try again." << endl;
 		}
-		cout << "do you want to do more : yes :: no" << endl;
+
+		cout << "\nDo you want to continue? (yes / no): ";
 		cin >> ch;
 	}
 
-
+	return 0;
 }
